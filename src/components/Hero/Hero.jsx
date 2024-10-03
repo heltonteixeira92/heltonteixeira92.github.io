ad6bb3bba8116c8c0ea84afa935c66f7cc276eb5
Helton Teixeira de Souza <heltonteixeiradesouza@hotmail.com>
2024-10-03 14:30:01 -0300

    hero section enhanced

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className={styles.content} >
                 <h1 className={styles.title} >Hi, I'm Helton</h1>
                 <p className={styles.description} >I'm a back-end developer, I help companies create scalable and efficient web applications.</p>
-                <a href="mailto:heltonteixeiradesouza@hotmail.com" className={styles.contactBtn} >Contact Me</a>
+                <a href="mailto:heltonteixeiradesouza@hotmail.com" className={styles.contactBtn} >Hire me</a>
             </div>
             <img src={getImageUrl("hero/Profile.jpeg")} alt="Hero image of me" className={styles.heroImg}/>
             <div className={styles.topBlur}/>
